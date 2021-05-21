3775329cb74ba912182a0f49baabc23c5ff102f9
Francis <francis.kim@travelflan.com>
2021-05-21 15:14:07 +0900

    ADD: CD

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
@@ -19,3 +20,5 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+handler = Mangum(app=app)
